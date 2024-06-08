7e5bef280862413da813378c5bd28a4c1e33574a
Atharv Phirke <148974367+Havok17122004@users.noreply.github.com>
2024-06-08 13:07:23 +0530

    Update soln.py

--- AtharvPhirke_230238/Assignment 2/soln.py
+++ AtharvPhirke_230238/Assignment 2/soln.py
@@ -1,3 +1,5 @@
+import sys
+
 input_file = open('cipher.txt','r')
 input_str = input_file.read()
 input_file.close()
@@ -5,73 +7,63 @@ input_file.close()
 
 output_file = open("plain.txt", "a")
 
+arr = []
 
-key = "\0" 
+for i in range (0, len(input_str), 2) :
 
-inputlen = len(input_str)
-# inputlen = 5
+    arr.append(int(input_str[i:i+2], 16))
 
-def encode(i, c, keylen) :
-    return ord(c) ^ ord(key[i % keylen])
+# print(arr)
+# max = 0
+# maxInd = -1
+# totals = []
+# for keylen in range (1, 10):
+#     count = [0] * 256
+#     for c in range (keylen):
+#         count[arr[c]] += 1
 
+#     sq_counts = []
+#     for i in count :
+#         sq_counts.append((i/sum(count)) ** 2)
+#     totals.append(round(sum(sq_counts), 4))
 
-# def genKey(c) :
-#     global key
-#     if ord(key[c]) >= 255:
-#         # key[c] = "\0"
-#         key = key[:c-1] + "\0" + key[c+1:]
-#         if len(key) == c+1:
-#             key = key + chr(1)
-#         else :
-#             # key[c+1] += 1
-#             if ord(key[c+1]) >= 255 :
-#                 genKey(c+1)
-#             else :
-#                 key = key[:c] + chr(ord(key[c+1]) + 1) + key[c+2:]
-#     return 
+# print(totals)
+# print(maxInd)
 
-def genKey(c):
-    global key
-    if ord(key[c]) >= 256:
-        key = key[:c] + "\0" + key[c+1:]
-        if c < len(key) - 1:
-            if ord(key[c+1]) >= 256:
-                genKey(c+1)  
-            else:
-                key = key[:c+1] + chr(ord(key[c+1]) + 1) + key[c+2:] 
-        elif c+1 == len(key):
-            key = key + chr(1)
-    return
+totals = []
+for k in range (1, 14): # why is range not upto len(arr)? can be limited only till 14
+    count = [0]*256
+    for i in arr[::k]:
+        count[i] += 1
+    
+    sq_counts = [(i/sum(count)) ** 2 for i in count] # what does this line mean!?
+    totals.append(round(sum(sq_counts), 4))
+# print(totals)
 
-
-def printASCII(key, keylen) :
-    for i in range(keylen) :
-        print(str(ord(key[i])), end = " ")
-    return
-
-for i in range (256 ** inputlen - 1):
-    if ord(key[0]) >= 256 :
-        genKey(0)
-    else :
-        if(len(key) == 1) :
-            key = chr(ord(key[0]) + 1)
-        else :
-            key = chr(ord(key[0]) + 1) + key[1:]
-
-    genKey(0)
-    keylen = len(key)
-    printASCII(key, keylen)
-    print()
-    output_file.write("0x")
-    for i in range(inputlen) :
-        # if want to represent the code in hex code uncomment this-
-    #   output_file.write(str(hex(encode(i, input_str[i], keylen)) [2:]))
-    #   output_file.write(" ")
-
-        # if want to include only the chars of the keyboard ->
-        c = encode(i, input_str[i], keylen)
-        if c >= 126 or c <= 31 :
-            break
-        output_file.write(chr(c))
-        
-    output_file.write("\n")
+keylen = totals.index(max(totals)) + 1
+key = []
+for keyletter in range (keylen):
+    letter = []
+    j = []
+    for i in range (keyletter, len(arr), keylen):
+        j.append(arr[i])
+    for b in range (256):
+        valid = True
+        ch = []
+        countAll = [0]*256
+        for x in j:
+            num = b^x
+            ch.append(num)
+            if num >= 256 :
+                break
+            
+            countAll[num] += 1
+        sq = [(i/sum(countAll)) ** 2 for i in countAll]
+        sumsq = sum(sq)
+        print(sumsq)
+        if min(ch) >= 32 and max(ch) <= 127 and countAll[ord('e')]/sum(countAll) > 0.10 and countAll[ord('t')]/sum(countAll) > 0.07 and 0.055 <= sumsq and sumsq <= 0.075:
+            letter.append(ch)
+    
+    key.append(letter)
+print(key)
+    
